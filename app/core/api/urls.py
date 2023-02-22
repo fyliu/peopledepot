5002985b3eebf434b4389bbef55c8054ea00028b
Fang Yi Liu <fyliu@users.noreply.github.com>
2023-02-22 11:17:07 -0800

    fix: collection endpoint name should be plural

--- app/core/api/urls.py
+++ app/core/api/urls.py
@@ -17,7 +17,7 @@ router.register(r"recurring-events", RecurringEventViewSet, basename="recurring-
 router.register(r"sponsor-partners", SponsorPartnerViewSet, basename="sponsor-partner")
 router.register(r"faqs", FaqViewSet, basename="faq")
 router.register(r"faqs-viewed", FaqViewedViewSet, basename="faq-viewed")
-router.register(r"location", LocationViewSet, basename="location")
+router.register(r"locations", LocationViewSet, basename="location")
 
 urlpatterns = [
     path("me/", UserProfileAPIView.as_view(), name="my_profile"),
